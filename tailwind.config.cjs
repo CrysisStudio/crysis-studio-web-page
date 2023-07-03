1a3a2ce6d166a7836c8aa501c1768ea8adf504e6
Facundo Carrion <facundojcarrion@gmail.com>
2023-07-03 16:44:48 -0300

    feat: add header

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      colors: {
+        red: '#ff4141',
+        dark: '#212121'
+      }
+    }
+  },
+  plugins: []
 }
