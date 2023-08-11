b2c65a21085053fd05461b4d3816227252e3daf6
Facundo Carrion <facundojcarrion@gmail.com>
2023-08-10 21:53:00 -0300

    feat: change roles

--- src/components/FilterButton.svelte
+++ src/components/FilterButton.svelte
@@ -7,7 +7,7 @@
 <button
   class="py-2 px-4 text-white border-2 rounded-full hover:text-rojo hover:border-rojo transition-color duration-300 ease-in-out
   {active === role
-    ? 'bg-red border-red hover:text-white'
+    ? 'bg-rojo border-rojo hover:text-white'
     : 'bg-transparent border-white'}"
   type="button"
   on:click={() => handleClick(role)}
